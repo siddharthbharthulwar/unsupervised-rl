b903b1a9def7b214ab3494637985ba1d8981f8d8
Siddharth Bharthulwar <sbharthulwar@college.harvard.edu>
2023-12-14 15:02:04 -0500

    add training history

--- pretrain.py
+++ pretrain.py
@@ -8,14 +8,36 @@ from torch.distributions.normal import Normal
 from diayn import DIAYN
 import gymnasium as gym
 from env_wrapper import EnvWrapper
+from datetime import datetime
+
+
+def add_training_run(d_arch, p_arch, env_name, epochs, num_skills):
+
+    now = datetime.now()
+    current_datetime = now.strftime("%m/%d/%Y, %H:%M:%S")
+
+    file = open("training_runs.txt", "a+")
+    file.seek(0)
+
+    data = file.read(100)
+    if len(data) > 0:
+        file.write("\n")
+
+    serialized_run = str(current_datetime) + ", "+ str(d_arch) + ", " + str(p_arch) + ", " + env_name + ", " + str(epochs) + ", " + str(num_skills)
+
+    file.write(serialized_run)
+
 
 ENV = "2dbox"
 INFO = { #this only matters for box2d env
     "xbounds": [-100, 100],
     "ybounds": [-100, 100]
 } 
-EPOCHS = 2000
+EPOCHS = 20
 NUM_SKILLS = 10
+
+DISCRIMINATOR_ARCH = [16, 8]
+POLICY_ARCH = [16, 8]
 
 envwrapper = EnvWrapper(ENV, INFO)
 env = envwrapper.env
@@ -23,7 +45,7 @@ last_path_return = 0
 max_path_return = -1 * np.inf
 num_episodes = 0
 
-agent = DIAYN(NUM_SKILLS, envwrapper.obs_space_dims, envwrapper.action_space_dims, [8, 8], [8, 8])
+agent = DIAYN(NUM_SKILLS, envwrapper.obs_space_dims, envwrapper.action_space_dims, DISCRIMINATOR_ARCH, POLICY_ARCH)
 discriminator_losses = []
 policy_losses = []
 
@@ -56,4 +78,7 @@ plt.savefig("diayn_plots/" + ENV + "pol.png")
 np.save("diayn_plots/" + ENV + "pol.npy", policy_losses)
 
 agent.save_state_dict(ENV)
+add_training_run(DISCRIMINATOR_ARCH, POLICY_ARCH, ENV, EPOCHS, NUM_SKILLS)
+
+
 
